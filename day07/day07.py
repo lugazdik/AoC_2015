63b42b8c3c9e02841676cf87c3ded25f0c0474f7
Lukas Gazdik <lugaz555@gmail.com>
2022-11-24 16:27:31 +0100

    day7 started

---
+++ day07/day07.py
@@ -0,0 +1,20 @@
+# link to source problem: https://adventofcode.com/2015/day/7
+
+
+def read_file(path):
+    result = []
+    with open(path, 'r') as file:
+        pass
+    return result
+
+
+def task01(parsed_input):
+    pass
+
+
+def task02(parsed_input):
+    pass
+
+prepared_file = read_file('day06.txt')
+print(task01(prepared_file))
+print(task02(prepared_file))
